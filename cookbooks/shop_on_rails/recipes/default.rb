d9d1320ec1cca61c8c24d0e14d32d3d7ae984438
Alexander Nеgоdа <alexander.negoda@gmail.com>
2013-10-20 20:47:41 +0400

    Disable restart for the hostaname s

--- cookbooks/shop_on_rails/recipes/default.rb
+++ cookbooks/shop_on_rails/recipes/default.rb
@@ -43,9 +43,10 @@ file '/etc/hostname' do
   content "#{hostname}\n"
 end
 
-service 'hostname' do
-  action :restart
-end
+#TODO fix for restart hostname service
+#service 'hostname' do
+#  action :restart
+#end
 
 file '/etc/hosts' do
   content "127.0.0.1 localhost #{hostname}\n"
